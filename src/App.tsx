907ae6294726ec23474c2a1e84b77839c62bd3cb
MRGhust <123163473+MRGhust@users.noreply.github.com>
2025-04-25 17:03:27 +0330

    Initial commit from your app

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Analytics />
+      <SpeedInsights />
+    </>
+  );
+}
+
+export default App;
